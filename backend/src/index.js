81a5aa4e587711416a49d055fab59e85f1a88430
User <revilo2242@gmail.com>
2025-04-17 12:57:13 +1000

    Fix: Prevent catch-all route from interfering with API routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,7 +32,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    if (!req.url.startsWith("/api")) {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    }
   });
 }
 
