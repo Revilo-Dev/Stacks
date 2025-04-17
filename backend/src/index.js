4af817d36f9a80a6007590fdcee5a017ecdda570
User <revilo2242@gmail.com>
2025-04-17 13:08:31 +1000

    Downgrade path-to-regexp to version 1.7.0

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
-
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -31,10 +28,8 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    if (!req.url.startsWith("/api")) {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    }
+  app.get("/:wildcard*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
